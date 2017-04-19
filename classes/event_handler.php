f6070483e271bf24c402c1a709f72bac6f8a2fd0
Luigi Serra <luigser@gmail.com>
2017-04-19 16:44:19 +0200

    Setting page

--- classes/event_handler.php
+++ classes/event_handler.php
@@ -151,41 +151,4 @@ class SPODNOTIFICATION_CLASS_EventHandler extends OW_ActionController
             }
         }
     }
-
-    public function sendEmailNotificationBatchProcess($frequency)
-    {
-        $mail_ready_to_send = array();
-        $notifications = SPODNOTIFICATION_BOL_Service::getInstance()->getAllNotifications();
-        foreach($notifications as $notification){
-            $users = SPODNOTIFICATION_BOL_Service::getInstance()->getRegisteredByPluginAndAction($notification->plugin ,$notification->action, $frequency);
-            foreach($users as $user){
-                $user = BOL_UserService::getInstance()->findUserById($user);
-                if ( empty($user) ) continue;
-                $data= json_decode($notification->data);
-                $mail = OW::getMailer()->createMail()
-                    ->addRecipientEmail($user->email)
-                    ->setHtmlContent($this->getEmailContentHtml($user->id, $data->message))
-                    ->setTextContent($this->getEmailContentText($data->message))
-                    ->setSubject($data->subject);
-            }
-            $ready = new stdClass();
-            $ready->mail = $mail;
-            $ready->notificationId = $notification->id;
-            array_push($mail_ready_to_send, $ready);
-        }
-
-        foreach($mail_ready_to_send as $mail){
-            try
-            {
-                //OW::getMailer()->send($mail->mail);
-                BOL_MailService::getInstance()->send($mail->mail);
-                SPODNOTIFICATION_BOL_Service::getInstance()->deleteNotificationById(intval($mail->notificationId));
-            }
-            catch ( Exception $e )
-            {
-                //Skip invalid notification
-            }
-        }
-
-    }
 }
