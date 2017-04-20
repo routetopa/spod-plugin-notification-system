f59cac39b538526cc182ab89d1cca0f370b20706
Luigi Serra <luigser@gmail.com>
2017-04-20 12:04:45 +0200

    Setting page

--- classes/consts.php
+++ classes/consts.php
@@ -2,7 +2,7 @@
 
 class SPODNOTIFICATION_CLASS_Consts
 {
-    const FREQUENCY_IMMEDIATELY = "1";
-    const FREQUENCY_EVERYDAY    = "2";
-    const FREQUENCY_EVERYMONTH  = "3";
+    const FREQUENCY_IMMEDIATELY = '1';
+    const FREQUENCY_EVERYDAY    = '2';
+    const FREQUENCY_EVERYMONTH  = '3';
 }
